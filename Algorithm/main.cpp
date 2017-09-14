33c26bfceffc40fb80ce82fb6af2a590f254f975
37801 <37801@DESKTOP-1651KFV>
2017-09-14 10:14:45 +0800

    添加了第26页的程序2-8

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -2,12 +2,14 @@
 #include<stdio.h>
 #include<time.h>
 
+#define LOCAL
+#define INF 1000000000
 using namespace std;
 
 void fun_2_2()  //存在乘法溢出的问题。
 {
 	int n, count = 0;
-	scanf_s("%d", &n, 1);
+	scanf_s("%d", &n);
 
 	while (n > 1)
 	{
@@ -70,7 +72,33 @@ void fun_2_3_separate_remain()
 	printf("Time used = %.2lf\n", (double)(finish - start) / CLOCKS_PER_SEC);
 }
 
+void fun_2_8()
+{
+	FILE *instream, *outstream;
+#ifdef LOCAL
+	freopen_s(&instream,"data.in.txt", "r", stdin);   //重定向标准输入输出;
+    freopen_s(&outstream,"data.out.txt", "w", stdout);
+	
+#endif // LOCAL
+	int a, sum = 0, max = -INF, min = INF, n = 0;
+
+	while (scanf_s("%d", &a) == 1)
+	{
+		if (a > max)
+			max = a;
+		else if (a < min)
+			min = a;
+		
+		sum += a;
+		//printf("a = %d, min = %d, max = %d", a, min, max);  
+		n++;
+	}
+	printf("%d %d %.3lf\n", min, max, ((double)sum) / n);
+}
+
+
 void main()
 {
+	fun_2_8();
 	system("pause");
 }
